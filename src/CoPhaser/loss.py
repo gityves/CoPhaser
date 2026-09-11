fixed rare nan gradients

src/CoPhaser/loss.py
@@ -470,7 +470,9 @@ class Loss:
         loss_dict["entropy_loss_unweighted"] = H_loss.item() / entropy_loss_weight
         loss_dict["entropy_loss"] = H_loss.item()
 
-        radii = torch.norm(inference_outputs["x_projected"][cycling_cells], dim=1)
+        radii = torch.sqrt(
+            (inference_outputs["x_projected"][cycling_cells] ** 2).sum(dim=1) + 1e-8
+        )
         radial_variance_loss = torch.var(radii) * closed_circle_weight
         circle_deviation_loss = torch.mean((radii - 1) ** 2) * closed_circle_weight
 

src/CoPhaser/model/rhythmic_encoder_VAE.py
@@ -53,7 +53,8 @@ class RhythmicEncoderVAE(FreezableModule):
         h = self.fc1(x_norm)
         h = self.fc2(h)
         x_projected = self.fc_mu(h)
-        mu = x_projected / x_projected.norm(dim=-1, keepdim=True)
+        norm = torch.sqrt((x_projected**2).sum(dim=-1, keepdim=True) + 1e-8)
+        mu = x_projected / norm
         return mu, x_projected
 
     def normalize_expression(